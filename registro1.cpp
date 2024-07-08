ced3c0ed6ee3eed71300e7d87b1a5d43ad686fea
Oscar <Oticonaf@unjbg.edu.pe>
2024-07-08 11:03:15 -0500

    Actualizar registro1.cpp

--- registro1.cpp
+++ registro1.cpp
@@ -18,13 +18,24 @@ int main(){
 	for(int i=0;i<n;i++){
 		cout<<"Ingrese el numero y nombre: "<<endl;
 		cin>>EMPLEADO[i].numero;
-		cin>>EMPLEADO[i].nombre;
+		cin.ignore();
+		getline(cin,EMPLEADO[i].nombre);
+		cout<<"Ingrese las ventas del empleado: "<<endl;
 		for(int j=0;j<12;j++){
-			cout<<"Ingrese las ventas del empleado: "<<endl;
 			cin>>EMPLEADO[i].ventas[j];
 		}
 		cout<<"Ingrese el salario"<<endl;
 		cin>>EMPLEADO[i].salario;
+	}
+	for(int i=0;i<n;i++){
+		cout<<"Numero: "<<EMPLEADO[i].numero<<endl;
+		cout<<"Nombre: "<<EMPLEADO[i].nombre<<endl;
+		cout<<"Ventas: ";
+		for(int j=0;j<12;j++){
+			cout<<EMPLEADO[i].ventas[j]<<" ";
+		}
+		cout<<endl;
+		cout<<"Salario: "<<EMPLEADO[i].salario<<endl;
 	}
 	return 0;
 }
